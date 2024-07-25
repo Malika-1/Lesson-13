d9b2b101b9564b5ab077ea7d72a98d86afd4db9b
Malika-1 <rabbimkulovamalika@gmail.com>
2024-07-25 11:48:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let numbers = [4, 16, 19, 22, 11, 144, 967, 19124];
+
+numbers.map((item) =>{
+    if(item % 2 === 0){
+      console.log('even');
+    }else{
+        console.log('odd');
+    }
+})
